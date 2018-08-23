d155db4701f7e5cc8a00133369e7af7d87065395
Tommy Muir <31448720+Muirey03@users.noreply.github.com>
2018-08-23 11:33:46 +0100

    Add whitelist to prefs

--- nebula/NBLWhitelistController.h
+++ nebula/NBLWhitelistController.h
@@ -1,5 +1,48 @@
 #import <Preferences/PSListController.h>
 
+@interface UIView (Nebula)
+-(id)_viewControllerForAncestor;
+@end
+
+@interface NSUserDefaults (Nebula) {
+}
+- (id)objectForKey:(id)key inDomain:(id)d;
+- (void)setObject:(id)obj forKey:(id)key inDomain:(id)d;
+@end
+@interface NSEntityDescription : NSObject {}
+  +(id)insertNewObjectForEntityForName:(id)name inManagedObjectContext:(id)object;
+@end
+
 @interface NBLWhitelistController : PSListController
 
+@end
+
+@interface WhitelistTableViewCell : UITableViewCell <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+{
+    UITableView* whitelistTable;
+    NSMutableArray* whitelistArray;
+}
+-(id)initWithSpecifier:(id)arg1;
+-(void)reloadTable;
+-(void)getWhitelistArray;
+-(void)setWhitelistArray;
+-(CGFloat)preferredHeightForWidth:(CGFloat)arg1;
+-(void)addButtonPressed;
+
+-(long long)numberOfSectionsInTableView:(id)arg1;
+-(long long)tableView:(id)arg1 numberOfRowsInSection:(long long)arg2;
+-(id)tableView:(id)arg1 cellForRowAtIndexPath:(id)arg2;
+-(double)tableView:(id)arg1 heightForRowAtIndexPath:(id)arg2;
+-(double)tableView:(id)arg1 heightForHeaderInSection:(long long)arg2;
+-(BOOL)tableView:(id)arg1 canEditRowAtIndexPath:(id)arg2;
+-(void)tableView:(id)arg1 commitEditingStyle:(long long)arg2 forRowAtIndexPath:(id)arg3;
+
+-(void)textFieldDidEndEditing:(id)arg1;
+-(void)textFieldDidBeginEditing:(id)arg1;
+@end
+
+@interface WhitelistCell : UITableViewCell
+@property (nonatomic, retain) UITextField* textField;
+@property (nonatomic, readwrite) NSInteger index;
+-(id)initWithStyle:(long long)arg1 reuseIdentifier:(id)arg2;
 @end
--- nebula/NBLWhitelistController.m
+++ nebula/NBLWhitelistController.m
@@ -1,5 +1,8 @@
 #include "NBLWhitelistController.h"
 
+#define kWidth [[UIApplication sharedApplication] keyWindow].frame.size.width
+#define kHeight [[UIApplication sharedApplication] keyWindow].frame.size.height
+
 @implementation NBLWhitelistController
 
 - (NSArray *)specifiers {
@@ -10,4 +13,145 @@
 	return _specifiers;
 }
 
+@end
+
+@implementation WhitelistTableViewCell
+- (id)initWithSpecifier:(PSSpecifier *)specifier
+{
+    self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    if (self)
+    {
+		//populate whitelist array
+		[self getWhitelistArray];
+
+        whitelistTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kWidth, whitelistArray.count * 45) style:UITableViewStylePlain];
+		whitelistTable.editing = YES;
+		whitelistTable.scrollEnabled = NO;
+
+        whitelistTable.delegate = self;
+        whitelistTable.dataSource = self;
+
+        [self addSubview:whitelistTable];
+    }
+	return self;
+}
+
+-(void)reloadTable
+{
+	[(UITableView*)[self superview] reloadData];
+	[whitelistTable reloadData];
+	whitelistTable.frame = CGRectMake(0, 0, kWidth, whitelistArray.count * 45);
+}
+
+-(void)getWhitelistArray
+{
+	whitelistArray = [[[NSUserDefaults standardUserDefaults] objectForKey:@"whitelistArray" inDomain:@"com.octodev.nebula"] mutableCopy];
+	if (!whitelistArray) { whitelistArray = [NSMutableArray new]; }
+}
+
+-(void)setWhitelistArray
+{
+	[[NSUserDefaults standardUserDefaults] setObject:[whitelistArray copy] forKey:@"whitelistArray" inDomain:@"com.octodev.nebula"];
+}
+
+-(void)didMoveToWindow
+{
+	[super didMoveToWindow];
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc]  initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addButtonPressed)];
+    [((NBLWhitelistController*)[[[self superview] superview] _viewControllerForAncestor]).navigationItem setRightBarButtonItem:addButton];
+}
+
+-(void)addButtonPressed
+{
+	[whitelistArray addObject:@""];
+	[self setWhitelistArray];
+	[self reloadTable];
+}
+
+-(CGFloat)preferredHeightForWidth:(CGFloat)arg1
+{
+    return whitelistArray.count * 45;
+}
+
+-(long long)numberOfSectionsInTableView:(id)arg1
+{
+	return 1;
+}
+
+-(long long)tableView:(id)arg1 numberOfRowsInSection:(long long)arg2
+{
+    return whitelistArray.count;
+}
+
+-(id)tableView:(id)arg1 cellForRowAtIndexPath:(NSIndexPath*)arg2
+{
+	WhitelistCell* cell = [[WhitelistCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier: @"myID"];
+	cell.index = arg2.row;
+	cell.textField.text = whitelistArray[arg2.row];
+
+	return cell;
+}
+
+//each cell will be 45 high
+-(double)tableView:(id)arg1 heightForRowAtIndexPath:(id)arg2
+{
+	return 45;
+}
+
+-(double)tableView:(id)arg1 heightForHeaderInSection:(long long)arg2
+{
+	return 0;
+}
+
+//allow editing of every cell
+-(BOOL)tableView:(id)arg1 canEditRowAtIndexPath:(id)arg2
+{
+	return YES;
+}
+
+//handle the deletion of cells
+-(void)tableView:(id)arg1 commitEditingStyle:(long long)arg2 forRowAtIndexPath:(NSIndexPath*)arg3
+{
+	[whitelistArray removeObjectAtIndex:arg3.row];
+	[self setWhitelistArray];
+	[self reloadTable];
+}
+
+-(void)textFieldDidEndEditing:(UITextField*)arg1
+{
+	NSInteger index = ((WhitelistCell*)[arg1 superview]).index;
+	NSString* text = arg1.text;
+	whitelistArray[index] = text;
+	[self setWhitelistArray];
+
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc]  initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addButtonPressed)];
+    [((NBLWhitelistController*)[[[self superview] superview] _viewControllerForAncestor]).navigationItem setRightBarButtonItem:addButton];
+	[self reloadTable];
+}
+
+-(void)textFieldDidBeginEditing:(UITextField*)arg1
+{
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStylePlain target:arg1 action:@selector(resignFirstResponder)];
+    [((NBLWhitelistController*)[[[self superview] superview] _viewControllerForAncestor]).navigationItem setRightBarButtonItem:addButton];
+}
+@end
+
+@implementation WhitelistCell
+-(id)initWithStyle:(long long)arg1 reuseIdentifier:(id)arg2
+{
+	self = [super initWithStyle:arg1 reuseIdentifier:arg2];
+	if (self)
+    {
+		_textField = [[UITextField alloc] initWithFrame:CGRectMake(50, 0, kWidth - 50, 45)];
+
+		[self addSubview:_textField];
+	}
+	return self;
+}
+
+-(void)didMoveToSuperview
+{
+	[super didMoveToSuperview];
+	_textField.delegate = (WhitelistTableViewCell*)[[self superview] superview];
+}
 @end
